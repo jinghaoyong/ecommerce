ce1110815b54c5fa1c0e6ff9ea8b79caa0c149a1
imperial <your.email@example.com>
2024-10-27 12:12:48 +0800

    ...

---
+++ src/app/shared/services/pagetitle/pagetitle.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+import { collection, doc, getDoc, getFirestore } from 'firebase/firestore';
+import { environment } from '../../../../environments/environment.development';
+// Initialize Firebase
+const app = initializeApp(environment.firebaseConfig);
+const db = getFirestore(app);
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PagetitleService {
+
+  constructor() { }
+
+  async getPageTitle(pageId: string): Promise<string | null> {
+    try {
+      const pagetitleRef = collection(db, `toptitle`);
+      const q = query(messagesRef, orderBy('createdAt', 'asc'));
+
+      onSnapshot(q, (querySnapshot) => {
+        const messages: Messages[] = querySnapshot.docs.map((doc: QueryDocumentSnapshot<DocumentData>) => doc.data() as Messages);
+        const unreadCount = this.countUnreadMessages(messages, participantId);
+        callback(unreadCount);
+      });
+
+    } catch (error: any) {
+      console.error('Error getting page title:', error.message);
+      throw error;
+    }
+  }
+
+}
