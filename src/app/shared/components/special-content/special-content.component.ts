191d91ad9d98d406b7019d3d15f1ce5a0a715bab
Sky Yong <jinghao0958@email.com>
2025-02-22 14:38:31 +0800

    skeleton for special content, and remember next time if want to use ngif ngfor need to import common module in your component page

--- src/app/shared/components/special-content/special-content.component.ts
+++ src/app/shared/components/special-content/special-content.component.ts
@@ -1,17 +1,21 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { SpecialContentService } from '../../../core/services/special-content/special-content.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-special-content',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './special-content.component.html',
   styleUrl: './special-content.component.scss'
 })
 export class SpecialContentComponent implements OnInit, OnDestroy {
+  allSpecialContents?: any[];
+
   specialContent1?: any;
   specialContent2?: any;
   specialContent3?: any;
+
   constructor(
     private specialContentServ: SpecialContentService
   ) {
@@ -20,11 +24,11 @@ export class SpecialContentComponent implements OnInit, OnDestroy {
 
   async loadSpecialContents() {
     try {
-      const allSpecialContents = await this.specialContentServ.getSpecialContent();
-      this.specialContent1 = allSpecialContents.filter(content => content.tier === 1);
-      this.specialContent2 = allSpecialContents.filter(content => content.tier === 2);
-      this.specialContent3 = allSpecialContents.filter(content => content.tier === 3);
-      console.log("special content 123",this.specialContent1,this.specialContent2,this.specialContent3)
+      this.allSpecialContents = await this.specialContentServ.getSpecialContent();
+      this.specialContent1 = this.allSpecialContents.filter(content => content.tier === 1);
+      this.specialContent2 = this.allSpecialContents.filter(content => content.tier === 2);
+      this.specialContent3 = this.allSpecialContents.filter(content => content.tier === 3);
+      console.log("special content 123", this.specialContent1, this.specialContent2, this.specialContent3)
     } catch (error) {
       console.error('Error loading special contents:', error);
     }
