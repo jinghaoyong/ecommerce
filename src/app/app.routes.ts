7a10e36d44190f652e5ff4d9b432d20de99e4dff
Sky Yong <jinghao0958@email.com>
2025-03-13 23:01:58 +0800

    searchresults page..

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { HomepageComponent } from './shared/pages/homepage/homepage.component';
 import { authGuard } from './account/auth/auth.guard';
 import { ProductDetailsComponent } from './shared/pages/product-details/product-details.component';
+import { SearchResultsComponent } from './shared/pages/search-results/search-results.component';
 
 export const routes: Routes = [
-    { path: '', component: HomepageComponent, canActivate: [authGuard], },
-    { path: 'pd', component: ProductDetailsComponent, canActivate: [authGuard], }
+    { path: '', component: HomepageComponent},
+    { path: 'productdetails', component: ProductDetailsComponent, canActivate: [authGuard], },
+    { path: 'searchresults', component: SearchResultsComponent, canActivate: [authGuard], }
 ];
