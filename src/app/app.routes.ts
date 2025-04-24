5883aaf69dc96e8bbbf8ebfa235ad60130435814
Sky Yong <jinghao0958@email.com>
2025-04-24 22:44:18 +0800

    create favourite page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { HomepageComponent } from './shared/pages/homepage/homepage.component';
 import { authGuard } from './account/auth/auth.guard';
 import { ProductDetailsComponent } from './shared/pages/product-details/product-details.component';
 import { SearchResultsComponent } from './shared/pages/search-results/search-results.component';
+import { FavouriteComponent } from './shared/pages/favourite/favourite.component';
 
 export const routes: Routes = [
     { path: '', component: HomepageComponent},
     { path: 'productdetails', component: ProductDetailsComponent, canActivate: [authGuard], },
-    { path: 'searchresults', component: SearchResultsComponent, canActivate: [authGuard], }
+    { path: 'searchresults', component: SearchResultsComponent, canActivate: [authGuard], },
+    { path: 'favourite', component: FavouriteComponent, canActivate: [authGuard], }
 ];
