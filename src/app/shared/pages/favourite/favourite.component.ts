d36e271a323f5355fe4e75ae050925737e14f8f5
Sky Yong <jinghao0958@email.com>
2025-04-30 22:59:38 +0800

    shopping card (favourite)

--- src/app/shared/pages/favourite/favourite.component.ts
+++ src/app/shared/pages/favourite/favourite.component.ts
@@ -1,16 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { LocalstorageService } from '../../services/localstorage/localstorage.service';
 import { FirebaseService } from '../../../core/services/firebase.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-favourite',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './favourite.component.html',
   styleUrl: './favourite.component.scss'
 })
 export class FavouriteComponent implements OnInit {
   // This component is responsible for displaying the favourite items of the user.  
+  cartItems = [
+    {
+      id: 1,
+      name: 'Wireless Headphones',
+      variation: 'Color: Black',
+      image: 'https://via.placeholder.com/60',
+      price: 59.99,
+      quantity: 1
+    },
+    {
+      id: 2,
+      name: 'Bluetooth Speaker',
+      variation: 'Color: Blue',
+      image: 'https://via.placeholder.com/60',
+      price: 89.50,
+      quantity: 2
+    },
+    {
+      id: 3,
+      name: 'Smartwatch Series 5',
+      variation: 'Size: Medium',
+      image: 'https://via.placeholder.com/60',
+      price: 120.00,
+      quantity: 1
+    }
+  ];
+
   constructor(
     private localStorageServ: LocalstorageService,
     private firebaseServ: FirebaseService
@@ -25,5 +53,9 @@ export class FavouriteComponent implements OnInit {
     this.firebaseServ.getUserDataById(currentUser?.userId).then((userData) => {
       console.log("getUserDataById userData", userData);
     });
+  }
+
+  getTotalPrice(item: any): number {
+    return item.price * item.quantity;
   }
 }
